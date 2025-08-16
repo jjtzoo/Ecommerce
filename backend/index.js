e941640a0fdc7942b85b5e9b32189e0e1cbe193e
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-08-16 22:17:09 +0800

    feat(routes): add userRoutes and mount in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+import userRoutes from "./routes/userRoutes.js"
+
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +19,8 @@ connectDB();
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
+
+app.use("api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
