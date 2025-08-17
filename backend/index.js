261502e11705208b240636d98e2094982eae89f6
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-08-17 15:51:12 +0800

    fix(userRoutes, index): fixed syntax errors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-app.use("api/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
